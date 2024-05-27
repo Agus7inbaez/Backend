f9cf39a95330f27dad01186fb65aef3fb73cbd23
agustin baez <Agus7inbaez@gmail.com>
2024-05-27 15:53:31 -0300

    segunda entrega

--- src/managers/products.manager.js
+++ src/managers/products.manager.js
@@ -1,5 +1,3 @@
-// imports
-
 import fs from "fs";
 import { v4 as uuid } from "uuid";
 
@@ -16,7 +14,7 @@ class ProducstManager {
     try {
       if (fs.existsSync(this.path)) {
         const products = await fs.promises.readFile(this.path, "utf8");
-        return querylimit ? JSON.parse(products).slice(0, querylimit) : JSON.parse(products); 
+        return querylimit ? JSON.parse(products).slice(0, Math.max(0, querylimit)) : JSON.parse(products); 
       } else return [];
     } catch (error) {
       console.error(error);
@@ -33,6 +31,7 @@ class ProducstManager {
       const products = await this.getProducts();
       products.push(newProduct);
       await fs.promises.writeFile(this.path,JSON.stringify(products, null, "\t"));
+      console.log("producto agregado");
       return newProduct;
     } catch (error) {
       console.error(error);
